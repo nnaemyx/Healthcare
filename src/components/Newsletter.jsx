79a734c028b56865697d0ee307e92e71b82c1643
tezon <tezonteam@gmail.com>
2022-10-19 03:55:58 -0700

    new

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const Newsletter = () => {
   return (
-    <div className="container mx-auto mt-8 ">
-      <div className="bg-primary rounded-md -ml-3 w-[110%] lg:w-[90%] py-4 sm:py-6 lg:mx-2">
+    <div className="container mx-auto mt-12 ">
+      <div className="bg-primary rounded-md -ml-3 sm:ml-0 w-[110%] md:w-[100%] lg:w-[90%] py-4 sm:py-6 lg:mx-2">
         <div className="flex lg:flex-row space-y-4  sm:space-y-0 md:flex-row flex-col px-2  justify-between">
-          <div className=" lg:mx-6  text-[12px] sm:text-[18px] w-[100%] md:w-[47%]  text-white lg:w-[35%]">
+          <div className=" lg:mx-6  text-[12px] md:text-[16px] md:mx-2 lg:text-[18px] w-[100%] md:w-[47%]  text-white lg:w-[35%]">
             <h1>
               Subcribe to our News letter to stay updated with our new health
               insurance plans{" "}
             </h1>
           </div>
 
-          <div className="flex justify-between px-2 rounded-md py-1 sm:py-3 md:w-[50%] lg:w-[40%] bg-white">
-            <div className="lg:py-2 lg:mx-4">
+          <div className="flex justify-between px-2  rounded-md py-1 sm:py-3 md:w-[50%] lg:w-[40%] bg-white">
+            <div className="lg:py-2 lg:mx-2">
               <input
                 type="email"
                 placeholder="Enter your email address"
@@ -22,8 +22,8 @@ const Newsletter = () => {
                 className="focus:outline-none"
               />
             </div>
-            <div className="bg-primary rounded-sm text-[12px] sm:text-[18px] text-white">
-              <button className="lg:px-6 md:px-4 md:py-1 py-1 px-3 lg:py-2">Subscribe</button>
+            <div className="bg-primary rounded-sm text-[12px] md:text-[16px] lg:text-[18px] text-white">
+              <button className="lg:px-6  md:px-4 md:py-1 py-1 px-3 lg:py-2">Subscribe</button>
             </div>
           </div>
         </div>
